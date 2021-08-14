077e93f7a722bcbbffc9e322155db6a2271d2464
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-08-14 12:58:45 +0200

    slider binding

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <div class="flex">
-      <select v-model="value" class="bg-black lg:hidden flex-shrink h-10 m-1 mr-0 mt-0 px-2 rounded-full text-white">
+      <select v-bind:value="value" @input="$emit('input', parseInt($event.target.value))" class="bg-black lg:hidden flex-shrink h-10 m-1 mr-0 mt-0 px-2 rounded-full text-white">
         <option v-for="(marker, key) in markers" :value="key" :key="key">{{ marker }}</option>
       </select>
-      <div class="bg-orange flex-grow h-10 m-1 mt-0 relative rounded-full">
+      <div class="group-hover:bg-orange flex-grow h-10 m-1 mt-0 relative rounded-full transition duration-100">
         <span class="absolute bg-black cursor-grab active:cursor-grabbing h-10 leading-10 px-3 rounded-full text-white top-1/2 transform -translate-y-1/2 z-10" :style="`left: ${100 * value / max}%; --tw-translate-x: ${-100 * value / max}%;`">{{ label }}</span>
-        <span class="absolute cursor-pointer hidden lg:inline px-3 top-1/2 transform -translate-y-1/2" v-for="(marker, key) in markers"
+        <span class="absolute cursor-pointer hidden lg:inline opacity-0 group-hover:opacity-100 px-3 top-1/2 transform -translate-y-1/2" v-for="(marker, key) in markers"
               :key="key"
               :style="`left: ${100 * key / max}%; --tw-translate-x: ${-100 * key / max}%;`"
-              @click="value = parseInt(key)">{{ marker }}</span>
+              @click="$emit('input', parseInt(key))">{{ marker }}</span>
       </div>
     </div>
   </div>
@@ -31,11 +31,6 @@ export default {
       } else {
         return this.value
       }
-    }
-  },
-  watch: {
-    value(value) {
-      this.$emit('update:value', value)
     }
   }
 }
